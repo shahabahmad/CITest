eb3d8aef6128d6933ed66f7d19708880467fd108
Shahab <shahab_ahmad@apple.com>
2019-04-25 15:56:00 +0530

    Adding base project
    AddingHead

---
+++ TestCIApp/TestCIApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestCIApp
+//
+//  Created by Shahab Ahmad on 25/04/19.
+//  Copyright © 2019 Shahab Ahmad. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
