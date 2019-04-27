719e1e794f0a673c654536857de8f4fc69cf87f3
Shahab <shahab_ahmad@apple.com>
2019-04-27 18:54:03 +0530

    dev team and commented unit test

--- TestCIApp/TestCIAppTests/TestCIAppTests.m
+++ TestCIApp/TestCIAppTests/TestCIAppTests.m
@@ -25,13 +25,14 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    XCTAssert(true)
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
+//- (void)testPerformanceExample {
+//    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
+//}
 
 @end
